5de543d931679fc93ee8090c1f542080f8834426
degun <pretty.lonely.place@hotmail.com>
2021-05-25 21:55:56 +0200

    small fix on contacts width

--- .eleventy.js
+++ .eleventy.js
@@ -90,6 +90,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
+  eleventyConfig.addPassthroughCopy("robots.txt");
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
